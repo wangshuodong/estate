b2c6e35d3e02ee5ceee24445cfbc84e1e56f1a58
wangshuodong <wawngshuodong2004@163.com>
2017-11-07 15:36:14 +0800

    wangsd

---
+++ src/main/java/com/wangsd/web/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.wangsd.web.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 用户管理
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private static final Logger logger = LogManager.getLogger(UserController.class.getName());
+
+    /**
+     * 用户登出
+     *
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpSession session) {
+        session.removeAttribute("userInfo");
+        // 登出操作
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        return "login";
+    }
+
+}
